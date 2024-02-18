36316768f5aa7c7e730dbd771b3e9f8bb374676a
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-02-18 17:46:38 +0530

    create first api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 const app = express()
+
+app.get("/api/chat" , (req, res) => {
+    res.send(chats)
+})
+
+app.get("/api/chat/:id", (req, res) => {
+  res.send(req);
+});
 
 app.listen(5000, console.log("server is startted on Port 5000"));
