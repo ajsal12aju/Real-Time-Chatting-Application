f50c3097cd529df6e9088df87afff44c3de1cd0f
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-03-05 20:14:42 +0530

    updated

--- frontend/src/pages/ChatPage.js
+++ frontend/src/pages/ChatPage.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 function ChatPage() {
 
+cosnt [chats, setChats] = useState([])
+
    const fetchChats = async () =>{
     const data = await axios.get("/api/chat");
+    setChats(data)
     console.log(data);
    }
 
