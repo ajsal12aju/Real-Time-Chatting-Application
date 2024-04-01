9d03275818ed5aa1e1b91284ae54cf80ab86c3ab
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-01 21:35:44 +0530

    updated

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,28 +1,41 @@
-import { Box, Container, Text } from '@chakra-ui/react'
+import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function HomePage() {
   return (
-   <Container maxW="xl" centerContent>
-    <Box
-    display="flex"
-    justifyContent="center"
-    p={"3"}
-    bg={"white"}
-    w="100%"
-    m="40px 0 15px 0"
-    borderRadius="lg"
-    borderWidth="1px"
-    >
-    <Text fontSize="4xl" fontFamily="work sans" color="black">
-      Sign Up
-    </Text>
-    </Box>
-<Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-
-</Box>
-   </Container>
-  )
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={"3"}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="work sans" color="black">
+          Sign Up
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px" color="black">
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList>
+            <Tab>Tab 1</Tab>
+            <Tab>Tab 2</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
 
 export default HomePage
