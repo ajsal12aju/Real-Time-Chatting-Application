95760da7f857c950546b8cf47032e2dc36dddda6
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-01 21:25:12 +0530

    updated

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,10 +1,27 @@
+import { Box, Container, Text } from '@chakra-ui/react'
 import React from 'react'
 
 function HomePage() {
   return (
-    <div>
-      welcome to home
-    </div>
+   <Container maxW="xl" centerContent>
+    <Box
+    d="flex"
+    justifyContent="center"
+    p={"3"}
+    bg={"white"}
+    w="100%"
+    m="40px 0 15px 0"
+    borderRadius="lg"
+    borderWidth="1px"
+    >
+    <Text fontSize="4xl" fontFamily="work sans" color="black">
+      Sign Up
+    </Text>
+    </Box>
+<Box>
+
+</Box>
+   </Container>
   )
 }
 
