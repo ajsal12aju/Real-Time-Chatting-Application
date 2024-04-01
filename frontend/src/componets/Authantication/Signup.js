f19312ce4452b760fbaea000fd6ccf5844a29623
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-01 22:52:46 +0530

    updated

--- frontend/src/componets/Authantication/Signup.js
+++ frontend/src/componets/Authantication/Signup.js
@@ -2,12 +2,21 @@ import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, V
 import React, { useState } from 'react'
 
 function Signup() {
+
+    const [show, setShow] = useState(false);
     const [name, setName] = useState()
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [confirmpassword, setConfirmpassword] = useState();
     const [pic, setPic] = useState();
 
+    const handleClick = () =>{
+        setShow(!show)
+    }
+    const postDetails = () => {
+
+    }
+  const submitHandler = () => {};
   return (
     <VStack color="black">
       <FormControl id="first-name" isRequired>
@@ -28,16 +37,28 @@ function Signup() {
         <FormLabel>Password</FormLabel>
         <InputGroup>
           <Input
+            type={show ? "text" : "password"}
             placeholder="Enter Your Password"
             onChange={(e) => setPassword(e.target.value)}
           />
-          <InputRightElement width='4.5rem'>
-          <Button h='1.75rem' size='sm' onClick={handleClick}>
-{show ? "Hide" : "show"}
-          </Button>
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "show"}
+            </Button>
           </InputRightElement>
         </InputGroup>
       </FormControl>
+      <FormControl id="pic" isRequired>
+        <FormLabel>Upload Your Picture</FormLabel>
+        <Input
+          type='file'
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+
+      <Button color='blue' style={{marginTop:15}} onClick={submitHandler}>
+
+      </Button>
     </VStack>
   );
 }
