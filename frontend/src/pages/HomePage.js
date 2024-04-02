854af0cb81f904f730f4b41f0bd8166c1be97102
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-02 23:32:06 +0530

    updated

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -18,7 +18,7 @@ function HomePage() {
         borderWidth="1px"
       >
         <Text fontSize="4xl" fontFamily="work sans" color="black">
-          Welcome To Chat App
+          Welcome To Chat Aplicatun
         </Text>
       </Box>
       <Box
