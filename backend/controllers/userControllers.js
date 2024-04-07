9478eaea09dc1e06b180cfd28b1d278c05b61402
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-07 15:09:32 +0530

    auhtantication statterd

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,39 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../modals/userModel")
+const registerUser = asyncHandler(async (req, res)=>{
+    const {name, email, password} = req.body
+
+    if(!name || !email || !password) {
+        res,status(400);
+        throw new Error("please Enter all the feilds")
+    }
+const userExisits =  await User.findOne({email})
+    if(userExisits){
+        res.status(400);
+        throw new Error("user is allready exists")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        pic
+    })
+
+    if(user){
+        res.status(201).json({
+            _id:user.id,
+            name:user.name,
+            email:email,
+            pic: user.pic
+        });
+
+    }else{
+        res.status(400);
+        throw new Error("user not found")
+    }
+
+    })
+
+
+    module.exports = {registerUser}
