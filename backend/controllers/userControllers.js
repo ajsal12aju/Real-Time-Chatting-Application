e397e63e9cb737e3412e460d7915dc4e5350554e
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-07 20:06:09 +0530

    auhtantication statterd

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const User = require("../modals/userModel")
 const generateToken = require("../config/generateToken")
 const registerUser = asyncHandler(async (req, res)=>{
-    const {name, email, password} = req.body
+    const {name, email, password, pic} = req.body
 
     if(!name || !email || !password) {
         res.status(400);
@@ -18,8 +18,8 @@ const userExisits =  await User.findOne({email})
         name,
         email,
         password,
-        pic
-    })
+         pic
+    })  
 
     if(user){
         res.status(201).json({
@@ -35,6 +35,26 @@ const userExisits =  await User.findOne({email})
         throw new Error("user not found")
     }
 
+    })
+
+    const authUser = asyncHandler(async (req, res)=>{
+        const {email, password} = req.body
+const user = await User.create({
+  email,
+});  
+
+         if (user) {
+           res.status(201).json({
+             _id: user.id,
+             name: user.name,
+             email: email,
+             pic: user.pic,
+             token: generateToken(user._id),
+           });
+         } else {
+           res.status(400);
+           throw new Error("Invalied user name and password");
+         }
     })
 
 
