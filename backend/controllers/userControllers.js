7a7fc8eada898f70e7c78b1e49aa529780bbf2f6
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-18 19:49:23 +0530

    updated

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -11,7 +11,7 @@ const registerUser = asyncHandler(async (req, res)=>{
 const userExisits =  await User.findOne({email})
     if(userExisits){
         res.status(400);
-        throw new Error("user is allready exists")
+        throw new Error("user is allready existss")
     }
     const user = await User.create({
         name,
@@ -33,7 +33,6 @@ const userExisits =  await User.findOne({email})
         res.status(400);
         throw new Error("user not found")
     }
-
     })
 
     const authUser = asyncHandler(async (req, res)=>{
