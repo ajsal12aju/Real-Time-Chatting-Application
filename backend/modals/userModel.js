94f3a1904c32e0cf95e5998bc5143b50ac1d205e
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-19 22:05:55 +0530

    password is hashed

--- backend/modals/userModel.js
+++ backend/modals/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 
 const userSchema = mongoose.Schema(
   {
@@ -18,6 +19,14 @@ const userSchema = mongoose.Schema(
   },
   { timestaps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if(!this.modified){
+    next()
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
 const User = mongoose.model("User", userSchema);
 
