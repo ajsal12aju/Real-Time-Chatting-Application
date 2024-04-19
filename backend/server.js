50039149d0066a8c5f046346267d92f268b76f70
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-19 22:59:26 +0530

    started registration

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const dotenv = require("dotenv")
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors")
-const userRoute = require('./routes/userRoutes')
+const userRoute = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
@@ -17,8 +18,8 @@ const PORT = process.env.PORT || 5000
 
 app.use("/api/user", userRoute);
 
-app.user(notFound)
-app.user(errorHandler)
+app.use(notFound)
+app.use(errorHandler)
 
 app.get("/api/chat" , (req, res) => {
     res.send(chats)
