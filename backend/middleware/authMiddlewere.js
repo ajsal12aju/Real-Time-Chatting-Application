0eb301c1b8c8a42c710a3acd13c3fe0899de2cc3
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-04-22 22:09:52 +0530

    updated

--- backend/middleware/authMiddlewere.js
+++ backend/middleware/authMiddlewere.js
@@ -10,8 +10,16 @@ const product = asyncHandler (async (req, res, next ) => {
             token = req.headers.authorization.split(" ")[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select("-password");
+            next()
         } catch (error) {
-            
+            res.status(401)
+            throw new Error("Not Autharaized , no token")
         }
     }
+    if(!token){
+        res.status(401);
+        throw new Error("Not Autharaized , no token");
+    }
 })
+
+module.exports = {product}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const {registerUser, authUser, allUsers} = require("../controllers/userControllers.js")
-const {protect} = require("../middleware/authMiddlewere.js")
+const { product } = require("../middleware/authMiddlewere.js");
 const router = express.Router()
 
 
-router.route("/").post(registerUser).get(protect, allUsers);
+router.route("/").post(registerUser).get(product, allUsers);
 router.post('/login', authUser)
 
 module.exports = router
