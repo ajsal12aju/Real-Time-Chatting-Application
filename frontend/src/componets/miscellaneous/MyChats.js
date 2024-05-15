16af641bc6d065cc42ed89142d625a73a9fae80f
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-05-15 21:40:35 +0530

    bug fix

--- frontend/src/componets/miscellaneous/MyChats.js
+++ frontend/src/componets/miscellaneous/MyChats.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { ChatState } from '../../Context/ChatProvider'
-import { Box, useToast } from '@chakra-ui/react'
+import { Box, Button, useToast } from '@chakra-ui/react'
 import axios from 'axios'
 
 function MyChats() {
@@ -37,11 +37,13 @@ fetchChats();
    },[])
 
   return (
-    <Box display={{base : selectedChat ? "none" : "flex", md: "flex"}} flexDirection="column" alignItems="center" p={3} bg="white" w={{base: "100%" , md:"31%"}} borderRadius="lg" borderWidth="1px" style={{color:'white'}}>
+    <Box display={{base : selectedChat ? "none" : "flex", md: "flex"}} flexDirection="column" alignItems="center" p={3} bg="white" w={{base: "100%" , md:"31%"}} borderRadius="lg" borderWidth="1px">
       <Box pb={3} px={3} fontSize={{base : "28px" , md: "30px"}} fontFamily="work sans" display="flex" w="100%" justifyContent="space-between" alignItems="center" >
+        My Chats
 
-
-
+        <Button display="flex" flexDir="column" p={3} bg="#F8F8F8" w="100%" h="100%" borderRadius="lg" overflow="hidden">  
+New Group Chat
+        </Button>
       </Box>
     </Box>
   )
