bf0f06a6ec070438c1fcb5641c7dbe600bcec061
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-05-19 18:51:51 +0530

    chat started

--- frontend/src/componets/miscellaneous/SingleChat.js
+++ frontend/src/componets/miscellaneous/SingleChat.js
@@ -1,25 +1,46 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, IconButton, Text } from '@chakra-ui/react'
 import React from 'react'
 import { ChatState } from '../../Context/ChatProvider';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 
 function SingleChat({ fetchAgain, setFetchAgain }) {
       const { selectedChat, setSelectedChat, user } = ChatState();
 
-  return <>
- {
-    selectedChat ? (
+  return (
+    <>
+      {selectedChat ? (
         <>
-        
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+          </Text>
         </>
-    ): (
-        <Box display="flex" alignItems="center" justifyContent="center" h="100%">
-            <Text fontSize="3xl" pb={3} fontFamily="Work sans">
-                Click on a user to start a chatting
-            </Text>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          h="100%"
+        >
+          <Text fontSize="3xl" pb={3} fontFamily="Work sans">
+            Click on a user to start a chatting
+          </Text>
         </Box>
-    )
- }
-  </>;
+      )}
+    </>
+  );
 }
 
 export default SingleChat
