cf26456915aceb45f45800f3bba725f030ca7333
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-05-19 14:44:58 +0530

    chat started

--- frontend/src/componets/miscellaneous/GroupChatModal.js
+++ frontend/src/componets/miscellaneous/GroupChatModal.js
@@ -11,20 +11,55 @@ import {
   Box,
   useDisclosure,
   useToast,
+  FormControl,
+  Input,
 } from "@chakra-ui/react";
 import { ChatState } from '../../Context/ChatProvider';
+import UserListItem from '../UserListItem'
+import axios from 'axios';
 function GroupChatModal({children}) {
   const { user, chats, setChats } = ChatState();
 
-    console.log(children, "===children==")
-      const { isOpen, onOpen, onClose } = useDisclosure();
-      const [groupChatName, setGroupChatName] = useState()
-      const [selectedUsers, setSelectedUsers] = useState([]);
-      const [search, setSearch] = useState("");
-      const [searchResult, setSearchResult] = useState([]);
-      const [loading, setLoading] = useState(false);
+  console.log(children, "===children==");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState();
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-      const toast = useToast()
+  const toast = useToast();
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+      console.log(data, "===data===");
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occurred",
+        description: "Failed to load the chat data.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top-left",
+      });
+    }
+  };
+  const handleSubmit = () => {};
+  const handleGroup = () => {};
+
+  ;
   return (
     <Box>
       <span onClick={onOpen}>{children}</span>
@@ -32,20 +67,47 @@ function GroupChatModal({children}) {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader 
-          fontSize="35px"
-          fontFamily="work sans"
-          display="flex"
-          justifyContent="center"
-          >Create Group Chat sdf</ModalHeader>
+          <ModalHeader
+            fontSize="35px"
+            fontFamily="work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            Create Group Chat
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody display="flex" flexDirection="column" alignItems="center" >
-          
+          <ModalBody display="flex" flexDirection="column" alignItems="center">
+            <FormControl>
+              <Input
+                placeholder="Chat Name"
+                mb={3}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder="Add Users eg: Jhon, ajsal"
+                mb={3}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
+            {loading ? (
+              <div>Loading.....</div>
+            ) : (
+              searchResult
+                ?.slice(0, 4)
+                .map((user) => (
+                  <UserListItem
+                    key={user._id}
+                    user={user}
+                    handleFunction={() => handleGroup(user)}
+                  />
+                ))
+            )}
           </ModalBody>
-
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
+            <Button colorScheme="blue" onClick={handleSubmit}>
+              Create Chat
             </Button>
             <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
