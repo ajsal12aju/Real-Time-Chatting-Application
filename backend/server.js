c03c384a6d04dd3944b43b108d9de8238c872aa9
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-07-08 20:38:15 +0530

    updated the data

--- backend/server.js
+++ backend/server.js
@@ -153,7 +153,7 @@ const io = require("socket.io")(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("Connected data   bn   d to socket.io");
+  console.log("Connected from here socket.io");
 
   socket.on("setup", (userData) => {
     socket.join(userData?._id);
