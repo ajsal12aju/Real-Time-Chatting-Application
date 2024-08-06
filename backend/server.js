783ba1661ba97af36e5c132a35b834bab0654dfb
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-08-06 20:35:44 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -170,7 +170,7 @@ io.on("connection", (socket) => {
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
   socket.on("new message", (newMessageReceived) => {
     var chat = newMessageReceived.chat;
-    if (!chat.users) return console.log("chat.usersg is not defined");
+    if (!chat.users) return console.log("chat.usersg dis not defined");
 
     chat.users.forEach((user) => {
       if (user._id == newMessageReceived.sender._id) return;
