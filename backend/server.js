09e18a66d3d6e17f2e273bae1f59081adf991471
Mohammed Ajsal <ajsal12aju@gmail.com>
2024-08-11 10:29:37 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -125,7 +125,7 @@ if (process.env.NODE_ENV === "production") {
   });  v             
 } else {
   app.get("/", (req, res) => {
-    res.send("API iS runningg updated correctyly  datas need to update ..  successfully");
+    res.send("API iS runningg updated correctylyddd fs  datas need to update ..  successfully");
   });
 }
 
